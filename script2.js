e472af119404e304b49d44ded23b0a1e057ba9e5
kent28808 <kktanoue@gmail.com>
2020-09-29 23:50:27 -0700

    cant get 5 day forecast to display

--- script2.js
+++ script2.js
@@ -41,8 +41,16 @@ function getForecast(searchvalue) {
         type: "GET",
         dataType: "json",
         success: function (data) {
-        
-
+        $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+    
+        var col = $("<div>").addClass("col-md-2");
+        var card = $("<div>").addClass("card bg-primary text-white");
+        var body = $("<div>").addClass("card-body p-2");
+        var title = $("<h5>").addClass("card-title").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+        var p1 = $("<p>").addClass("card-text").text("Temp: " + data.list[i].main.temp_max + " F");
+        var p2 = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
+        col.append(card.append(body.append(title, img, p1, p2)));
+            $("#forecast .row").append(col);
         }
 
 
