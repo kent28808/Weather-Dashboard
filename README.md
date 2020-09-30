3368209b9473f3c23cc9f0bf8a1bc07536e784c7
kent28808 <kktanoue@gmail.com>
2020-09-29 22:22:04 -0700

    revised JS

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # Weather-Dashboard
+
+## Description
+
+This is my fitth project, where I was tasked with creating a simple calendar application that allows the user to save events for each hour of the day.  
+This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+[Moment.js] was used to display the current date and time.
+
+![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
+
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://www.w3schools.com/css/)
+* [Bootstrap](https://getbootstrap.com/)
+* [JS](https://www.w3schools.com/js/)
+* [JQuery] (https://www.jquery.com)
+* [Moment.js] (https://www.momentjs.com)
+
+
+## Deployed Link
+
+* [Deployed link](https://kent28808.github.io/Schedule-planner/)
+
+
+## Authors
+
+* **Ken Tanoue** 
+
+- [Link to Portfolio Site](https://kent28808.github.io/Responsive-Portfolio/portfolio.html)
+- [Link to Github](https://github.com/kent28808/)
+
+## License
+
+MIT License
+
+Copyright (c) [2020] [Ken T.]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Acknowledgments
+
+* Hats off to instructional staff, and all technology libraries used.
--- script2.js
+++ script2.js
@@ -1,30 +1,52 @@
-$(document).ready(
-    function () {
-        $("#search-button").on("click", function () {
-            var search = $("#search-value").val()
-            dosearch(search)
-            $("#search-value").val("")
+$(document).ready(function () {
+    $("#search-button").on("click", function () {
+        var search = $("#search-value").val()
+        dosearch(search)
+        $("#search-value").val("")
+    })
+    
+    function dosearch(searchvalue) {
+        //console.log(searchvalue)
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?appid=298e7cec3267bf8dbf8d5c54ff6dc6a7&units=imperial&q=" + searchvalue,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                //console.log(data)
+               
+                $("#today").empty();
+                var card = $("<div>").addClass("card");
+                var cardbody = $("<div>").addClass("card-body");
+                var title = $("<h4>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+                var temperature = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp);
+                var units = $("<sup>").text("o");
+                var humidity = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + " %");
+                var windspeed = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH")
+
+                temperature.append(units, $("<span>").text(" F"))
+                cardbody.append(title, temperature, humidity, windspeed,)
+                card.append(cardbody)
+                $("#today").append(card)
+                //need to figure out how to add UV index.
+
+            }
         })
-        function dosearch (searchvalue) {
-            //console.log(searchvalue)
-            $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/weather?appid=298e7cec3267bf8dbf8d5c54ff6dc6a7&units=imperial&q="+searchvalue,
-                type: "GET",
-                dataType: "json",
-                success: function (data) {
-                    //console.log(data)
-                    $("#today").empty()
-                    var card = $("<div>").addClass("card")
-                    var cardbody = $("<div>").addClass("card-body")
-                    var title = $("<h3>").addClass("card-title").text(data.name)
-                    var temperature = $("<p>").addClass("card-text").text("Temperature: "+ data.main.temp)
-                    var units = $("<sup>").text("o")
-                    temperature.append(units, $("<span>").text(" F"))
-                    cardbody.append(title, temperature)
-                    card.append(cardbody)
-                    $("#today").append(card)
-                }
-            })
-        }
     }
-)
+}
+);
+
+function getForecast(searchvalue) {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?appid=298e7cec3267bf8dbf8d5c54ff6dc6a7&units=imperial&q=" + searchvalue,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+        $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");    
+        
+        }
+
+
+
+    })
+}
+
